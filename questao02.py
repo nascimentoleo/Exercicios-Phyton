f9b953100e074456481eeb36e8c79ff2def6c0df
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2016-03-20 14:48:51 -0300

    Lista 01

---
+++ questao02.py
@@ -0,0 +1,10 @@
+def lerInteiro(limiteMax):
+	m = []
+	for i in range(0,limiteMax):
+		m.append(input("Digite um numero \n"))
+	return m
+
+def imprimeInverso():
+	print(lerInteiro(10)[::-1])
+
+imprimeInverso()
---
+++ questao04.py
@@ -0,0 +1,21 @@
+def lerLetras(limiteMax):
+	m = []
+	for i in range(0,limiteMax):
+		m.append(input("Digite uma letra \n"))
+	return m
+
+def getConsoantes(expressao):
+	vogais = ['a','e','i','o','u']
+	consoantes = []
+	for letra in expressao:
+		if(letra not in vogais):
+			consoantes.append(letra)
+	return consoantes
+
+expressao = getConsoantes(lerLetras(10))
+print(expressao)
+print("Quantidade de consoantes " + str(len(expressao)))
+
+
+
+
