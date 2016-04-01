d46f2f2f968973401e29d1c92fc84a427ee92e01
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2016-04-01 12:47:57 -0300

    Fix bugs

--- Lista 03/questao03.py
+++ Lista 03/questao03.py
@@ -1,1 +1,13 @@
-usuarios = open("usuarios.txt")
+def lerUsuarios(nomeDoArquivo):
+	arquivoDeUsuarios = open("usuarios.txt")
+	usuarios = {}
+	for linha in arquivoDeUsuarios.readlines():
+		for palavra in arquivoDeUsuarios.split():
+			usuarios
+
+
+def gerarRelatorio(usuarios):
+
+
+
+
