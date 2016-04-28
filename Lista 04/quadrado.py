e199d710a2a238a4228c9ffcf3116735b84da54a
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2016-04-28 15:11:25 -0300

    Lista 04

---
+++ Lista 04/quadrado.py
@@ -0,0 +1,15 @@
+class Quadrado:
+	def __init__(self, lado):
+		self.lado = lado
+
+	def setLado(self, lado):
+		self.lado = lado
+
+	def getLado(self):
+		return self.lado
+
+	def calculaArea(self):
+		return self.lado * self.lado
+
+
+
---
+++ Lista 04/testequadrado.py
@@ -0,0 +1,7 @@
+from quadrado import *
+
+quadrado = Quadrado(4)
+print(quadrado.getLado())
+quadrado.setLado(5)
+print(quadrado.getLado())
+print(quadrado.calculaArea())
